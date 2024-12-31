3a5c33b2ba317b3cc8a45e2d6c9d644fa704ee21
Nikunj Ranja <nikunjranjan2204@gmail.com>
2024-12-31 16:17:04 +0530

    first commit

---
+++ src/data/coworkingSpaces.ts
@@ -0,0 +1,323 @@
+export interface CoworkingSpace {
+  name: string;
+  location: string;
+  category: string;
+  price: string;
+  coordinates: [number, number];
+  area: string;
+}
+
+export const coworkingSpaces: CoworkingSpace[] = [
+  {
+    name: "Coworking Space Guindy 1",
+    location: "Guindy",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.0108, 80.2206],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Anna Nagar 1",
+    location: "Anna Nagar",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [13.086, 80.2101],
+    area: "Chennai - South"
+  },
+  {
+    name: "Coworking Space Kesavaperumalpuram",
+    location: "Kesavaperumalpuram",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.033, 80.266],
+    area: "Chennai - South"
+  },
+  {
+    name: "Coworking Space Madipakkam",
+    location: "Madipakkam",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [12.962, 80.197],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Alwarpet",
+    location: "Alwarpet",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.0335, 80.251],
+    area: "Chennai - Central"
+  },
+  {
+    name: "Coworking Space Velachery",
+    location: "Velachery",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [12.9766, 80.221],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Mylapore",
+    location: "Mylapore",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [13.0339, 80.2699],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Mogappair East",
+    location: "Mogappair East",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [13.09, 80.173],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Anna Salai",
+    location: "Anna Salai",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.06, 80.2619],
+    area: "Chennai - Central"
+  },
+  {
+    name: "Coworking Space Royapettah",
+    location: "Royapettah",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.055, 80.266],
+    area: "Chennai - North"
+  },
+  {
+    name: "Coworking Space OMR",
+    location: "OMR",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [12.905, 80.2276],
+    area: "Chennai - South"
+  },
+  {
+    name: "Coworking Space Vadapalani",
+    location: "Vadapalani",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.05, 80.212],
+    area: "Chennai - South"
+  },
+  {
+    name: "Coworking Space Nungambakkam",
+    location: "Nungambakkam",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.06, 80.24],
+    area: "Chennai - West"
+  },
+  {
+    name: "Coworking Space Maduravoyal",
+    location: "Maduravoyal",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [13.07, 80.17],
+    area: "Chennai - West"
+  },
+  {
+    name: "Coworking Space Tambaram",
+    location: "Tambaram",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [12.9229, 80.1275],
+    area: "Chennai - South"
+  },
+  {
+    name: "Coworking Space Perungudi",
+    location: "Perungudi",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [12.9612, 80.241],
+    area: "Chennai - South"
+  },
+  {
+    name: "Coworking Space Nandanam",
+    location: "Nandanam",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.0301, 80.2414],
+    area: "Chennai - West"
+  },
+  {
+    name: "Coworking Space Teynampet",
+    location: "Teynampet",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.043, 80.251],
+    area: "Chennai - South"
+  },
+  {
+    name: "Coworking Space Greams Road",
+    location: "Greams Road",
+    category: "Premium(12K or More)",
+    price: "12000",
+    coordinates: [13.06, 80.25],
+    area: "Chennai - Central"
+  },
+  {
+    name: "Coworking Space Sholinganallur",
+    location: "sholinganallur",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [12.9, 80.227],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Saidapet",
+    location: "Saidapet",
+    category: "Premium(12K or More)",
+    price: "12000",
+    coordinates: [13.02, 80.22],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Kolathur",
+    location: "Kolathur",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [13.13, 80.22],
+    area: "Chennai - North"
+  },
+  {
+    name: "Coworking Space Besant Nagar",
+    location: "Besant nagar",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.00, 80.27],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Kodambakkam",
+    location: "Kodambakkam",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.0481, 80.2214],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Kilpauk",
+    location: "Kilpauk",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.08, 80.24],
+    area: "Chennai - West"
+  },
+  {
+    name: "Coworking Space Chetpet",
+    location: "Chetpet",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [13.07, 80.24],
+    area: "Chennai - North"
+  },
+  {
+    name: "Coworking Space Saligramam",
+    location: "Saligramam",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.05, 80.2],
+    area: "Chennai - North"
+  },
+  {
+    name: "Coworking Space Purasaiwakkam",
+    location: "Purasaiwakkam",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [13.09, 80.26],
+    area: "Chennai - West"
+  },
+  {
+    name: "Coworking Space Thiruvanmiyur",
+    location: "Thiruvanmiyur",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [12.98, 80.27],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Koyembedu",
+    location: "Koyembedu",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [13.07, 80.2],
+    area: "Chennai - North"
+  },
+  {
+    name: "Coworking Space Villivakkam",
+    location: "Villivakkam",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.12, 80.21],
+    area: "Chennai - North"
+  },
+  {
+    name: "Coworking Space Porur",
+    location: "Porur",
+    category: "Premium(12K or More)",
+    price: "12000",
+    coordinates: [13.0358, 80.1589],
+    area: "Chennai - North"
+  },
+  {
+    name: "Coworking Space Thoraipakkam",
+    location: "Thoraipakkam",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [12.93, 80.23],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Aminjikarai",
+    location: "Aminjikarai",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.07, 80.22],
+    area: "Chennai - North"
+  },
+  {
+    name: "Coworking Space Egmore",
+    location: "Egmore",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [13.0732, 80.2609],
+    area: "Chennai - South"
+  },
+  {
+    name: "Coworking Space Oragadam",
+    location: "oragadam",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.12, 80.15],
+    area: "Chennai - South"
+  },
+  {
+    name: "Coworking Space Ambattur",
+    location: "Ambattur",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.1, 80.16],
+    area: "Chennai - North"
+  },
+  {
+    name: "Coworking Space Ekkatuthangal",
+    location: "Ekkatuthangal",
+    category: "Sub premium (6k-12k)",
+    price: "6000",
+    coordinates: [13.01, 80.2],
+    area: "Chennai - East"
+  },
+  {
+    name: "Coworking Space Ambattur OT",
+    location: "Ambattur OT",
+    category: "Budgeted(6k or less)",
+    price: "6000",
+    coordinates: [13.11, 80.15],
+    area: "Chennai - North"
+  }
+];
